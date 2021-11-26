198d19d88ff39a3aad3e3f29b1d33098cf6f427c
gyan-js <gyan0304@gmail.com>
2021-11-26 13:16:07 +0530

    Initial Code Drop

--- App.js
+++ App.js
@@ -1,21 +1,19 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+//import from {SafeAreaProvider}
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import HomeScreen from './screens/HomeScreen'
+
+
+export default class App extends Component{
+  render(){
+    return(
+      <SafeAreaProvider>
+      <View style={{flex:1}}>
+        <HomeScreen />
+      </View>
+      </SafeAreaProvider>
+    )
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
